dab1baa4723cae734fc1dd89073517f48b1bf5ae
malagege <s9714020@gmail.com>
2025-01-19 17:04:04 +0800

    更新 CaddyBrowseDeleter 專案設定，調整專案識別碼及更新 Swashbuckle.AspNetCore 套件版本，並新增 HTTP 請求範本

--- src/CaddyBrowseDeleter/Program.cs
+++ src/CaddyBrowseDeleter/Program.cs
@@ -1,57 +1,48 @@
-using System;
+
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
-namespace CaddyBrowseDeleter;
-
-class Program
+namespace CaddyBrowseDeleter
 {
-    static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // 設定服務
-        builder.Services.AddDbContext<AppDbContext>();
-
-        // 加入 MVC 核心服務並啟用 API 探索
-        builder.Services.AddMvcCore()
-                        .AddApiExplorer();
-
-        
-        // 加入 Swagger 服務
-        builder.Services.AddSwaggerGen(c =>
+        public static void Main(string[] args)
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CaddyBrowseDeleter API", Version = "v1" });
-        });
+            var builder = WebApplication.CreateBuilder(args);
 
-        var app = builder.Build();
+            // Add services to the container.
+            // 設定服務
+            builder.Services.AddDbContext<AppDbContext>();
 
-        // 自動建立 Migration 並更新資料庫
-        using (var scope = app.Services.CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            dbContext.Database.Migrate();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            
+            // 自動建立 Migration 並更新資料庫
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.Migrate();
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            // app.UseHttpsRedirection();
+
+            // app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
         }
-
-        // 設定中介軟體
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-
-
-        // 啟用 Swagger 和 Swagger UI
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "CaddyBrowseDeleter API v1");
-        });
-        // 設定 API 控制器
-        app.MapControllers();
-
-        app.Run();
     }
 }
