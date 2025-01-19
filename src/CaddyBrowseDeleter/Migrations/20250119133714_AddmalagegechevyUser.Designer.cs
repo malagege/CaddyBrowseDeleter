4955ec0ec6579726683c706a74b01a9170d67361
malagege <s9714020@gmail.com>
2025-01-19 21:37:56 +0800

    新增遷移以插入使用者資料 "malagege" 和 "chevy"

--- src/CaddyBrowseDeleter/Migrations/20250119133714_AddmalagegechevyUser.Designer.cs
+++ src/CaddyBrowseDeleter/Migrations/20250119133714_AddmalagegechevyUser.Designer.cs
@@ -2,6 +2,7 @@
 using CaddyBrowseDeleter;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CaddyBrowseDeleter.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250119133714_AddmalagegechevyUser")]
+    partial class AddmalagegechevyUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
---
+++ src/CaddyBrowseDeleter/Migrations/20250119133714_AddmalagegechevyUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CaddyBrowseDeleter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddmalagegechevyUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "malagege" },
+                    { 2L, "chevy" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2L);
+        }
+    }
+}
