c0a8a8ea1ee416469ec2a12266fe110031267639
malagege <s9714020@gmail.com>
2025-01-19 22:23:40 +0800

    新增 JSON 選項以忽略循環引用，優化控制器的序列化處理

--- src/CaddyBrowseDeleter/Program.cs
+++ src/CaddyBrowseDeleter/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace CaddyBrowseDeleter
@@ -12,7 +14,11 @@ namespace CaddyBrowseDeleter
             // 設定服務
             builder.Services.AddDbContext<AppDbContext>();
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
